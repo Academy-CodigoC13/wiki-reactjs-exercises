571c03f6c8ac8db5c68c121aeff7747b92dd6a03
Lina Beltrán <lmbd92@hotmail.com>
2024-01-21 12:27:32 -0500

    Add SweetAlert2 library for displaying alerts

--- exercises/src/modules/api-request/Api.js
+++ exercises/src/modules/api-request/Api.js
@@ -1,16 +1,41 @@
 import React, { useState } from "react";
 import Axios from "axios";
+import showAlert from "../alert/Alert";
 import "./Api.css";
 
 const Api = () => {
   const [joke, setJoke] = useState("");
   const getJoke = () => {
-    Axios.get("https://official-joke-api.appspot.com/random_joke").then(
-      (response) => {
-        console.log(response);
+    Axios.get("https://official-joke-api.appspot.com/random_joke") // change the url to test the error
+      .then((response) => {
         setJoke(response.data.setup + " " + response.data.punchline);
-      }
-    );
+        if (response.data.setup === "") {
+          showAlert(
+            "top-end",
+            "error",
+            "Error!",
+            "The request could not be processed",
+            2000
+          );
+        } else {
+          showAlert(
+            "top-end",
+            "success",
+            "Success!",
+            "Joke successfully requested",
+            2000
+          );
+        }
+      })
+      .catch((error) => {
+        showAlert(
+          "top-end",
+          "error",
+          "Error!",
+          "The request could not be processed",
+          2000
+        );
+      });
   };
   return (
     <div className="Api">
